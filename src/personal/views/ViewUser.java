5709ebb4c548f61e14e87ea5d26c3bfbfc89b044
vlad722011 <vlad722011@yandex.ru>
2023-06-10 14:24:10 +0700

    добавили обработчик исключений при введении пользователем команды.

--- src/personal/views/ViewUser.java
+++ src/personal/views/ViewUser.java
@@ -18,24 +18,28 @@ public class ViewUser {
 
         while (true) {
             String command = prompt("Введите команду: ");
-            com = Commands.valueOf(command.toUpperCase());
-            if (com == Commands.EXIT) return;
-            switch (com) {
-                case CREATE:
-                    String firstName = prompt("Имя: ");
-                    String lastName = prompt("Фамилия: ");
-                    String phone = prompt("Номер телефона: ");
-                    userController.saveUser(new User(firstName, lastName, phone));
-                    break;
-                case READ:
-                    String id = prompt("Идентификатор пользователя: ");
-                    try {
-                        User user = userController.readUser(id);
-                        System.out.println(user);
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                    break;
+            try {
+                com = Commands.valueOf(command.toUpperCase());
+                if (com == Commands.EXIT) return;
+                switch (com) {
+                    case CREATE:
+                        String firstName = prompt("Имя: ");
+                        String lastName = prompt("Фамилия: ");
+                        String phone = prompt("Номер телефона: ");
+                        userController.saveUser(new User(firstName, lastName, phone));
+                        break;
+                    case READ:
+                        String id = prompt("Идентификатор пользователя: ");
+                        try {
+                            User user = userController.readUser(id);
+                            System.out.println(user);
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
         }
     }
